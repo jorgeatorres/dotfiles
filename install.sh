ebbdb2fd1f4ce101580ac2eddc9b34af83caf9e5
Jorge A. Torres <j@jorgetorres.co>
2019-06-17 01:49:29 -0300

    Reorganize SSH config

--- install.sh
+++ install.sh
@@ -87,9 +87,19 @@ done
 if needs_update "$DOTFILESDIR/ssh/id_rsa.pub" "$HOME/.ssh/id_rsa.pub"; then
 	mkdir -p "$HOME/.ssh"
 	rm -rfv "$HOME/.ssh/{id_rsa,id_rsa.pub}"
+
+cat <<EOT > "$HOME/.ssh/config"
+Host *
+  UseKeychain yes
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/id_rsa
+EOT
+
 	cp "$DOTFILESDIR/ssh/id_rsa.pub" "$HOME/.ssh/"
 	eval $(op signin my.1password.com j@jorgetorres.co)
 	op get document "7ukxxbzsincz3bqsx6p6iwl32e" > "$HOME/.ssh/id_rsa"
+	chmod 644 "$HOME/.ssh/id_rsa.pub"
+	chmod 600 "$HOME/.ssh/id_rsa"
 	ssh-add -K
 fi
 

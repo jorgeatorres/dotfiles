adebceb28d2006c30c0eb8408fd1f38d2eec92cd
Jorge A. Torres <jorge@0xbad.com>
2021-02-07 11:00:11 -0500

    Smarter Homebrew setup

--- _setup/homebrew.sh
+++ _setup/homebrew.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-if [[ ! $(which brew)  ]]; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Xcode is neede for a bunch of stuff.
+if [[ ! -e "/Applications/Xcode.app" ]]; then
+	echo "Please install Xcode from the App Store before proceeding!"
+	exit 1
 fi
 
+if [[ ! $(which brew)  ]]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	source ~/.zshrc
+fi
 
+# Homebrew requires Rosetta on Apple Silicon for some packages.
+if [ "$(uname -m)" = "arm64" ]; then
+	sudo softwareupdate --install-rosetta
+fi
+
+# Install packages.
 brew bundle check --file="$DIR/homebrew/Brewfile" >/dev/null 2>&1  || {
-    brew bundle --file="$DIR/homebrew/Brewfile"
+    brew bundle --file="$DIR/homebrew/Brewfile" --verbose
 }

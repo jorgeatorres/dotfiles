1cddcfe8bd6c2b3cc4f302be7377692ba0b86d7d
Jorge A. Torres <jorge@0xbad.com>
2021-02-07 07:11:24 -0500

    Update Homebrew install script URL

--- _setup/homebrew.sh
+++ _setup/homebrew.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
-if [[ ! -e "/usr/local/bin/brew" ]]; then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+if [[ ! $(which brew)  ]]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
 
 brew bundle check --file="$DIR/homebrew/Brewfile" >/dev/null 2>&1  || {
     brew bundle --file="$DIR/homebrew/Brewfile"

3d65e7f32c5d8483ef9648995169c45818343001
Jorge A. Torres <jorge@0xbad.com>
2021-02-07 10:56:54 -0500

    Change ‘bin’ directory to ‘.bin’

--- _setup/bin-private.sh
+++ _setup/bin-private.sh
@@ -4,8 +4,8 @@ if [[ ! -d "$DIR/bin-private.git" ]]; then
 fi
 
 if [[ -d "$DIR/bin-private.git" ]]; then
-	maybe_link "$DIR/bin-private.git/a8c.sh" "$HOME/bin/a8c-proxy"
-	maybe_link "$DIR/bin-private.git/nike-training.rb" "$HOME/bin/nike-training"
-	maybe_link "$DIR/bin-private.git/dayone-sync.py" "$HOME/bin/dayone-sync"
-	maybe_link "$DIR/bin-private.git/woo.php" "$HOME/bin/woo"
+	maybe_link "$DIR/bin-private.git/a8c.sh" "$HOME/.bin/a8c-proxy"
+	maybe_link "$DIR/bin-private.git/nike-training.rb" "$HOME/.bin/nike-training"
+	maybe_link "$DIR/bin-private.git/dayone-sync.py" "$HOME/.bin/dayone-sync"
+	maybe_link "$DIR/bin-private.git/woo.php" "$HOME/.bin/woo"
 fi

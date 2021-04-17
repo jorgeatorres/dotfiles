7ac0f1490e82c77f4c10a1e99fb4e65be8f3cdf3
Jorge A. Torres <jorge@0xbad.com>
2021-04-17 12:17:46 -0500

    Simplify MailHog setup script now that an M1 package exists

--- _setup/mailhog.sh
+++ _setup/mailhog.sh
@@ -1,12 +1,18 @@
+MAILHOG_PATH=`which MailHog`
+if [[ -n "$MAILHOG_PATH" ]]; then
+	if [ "$(uname -m)" = "arm64" ]; then
+		PHP_ETC="/opt/homebrew/etc/php"
+	else
+		PHP_ETC="/usr/local/etc/php"
+	fi
 
-if [[ $(which MailHog) ]]; then
-	for dir in /usr/local/etc/php/*; do
+	for dir in $PHP_ETC/*; do
 	    dir=${dir%*/}
 
 		if [ ! -e "$dir/conf.d/mailhog.ini" ]; then
 			cat <<- EOF > "$dir/conf.d/mailhog.ini"
 			[mail function]
-			sendmail_path = '/usr/local/bin/MailHog sendmail'
+			sendmail_path = '$MAILHOG_PATH sendmail'
 			EOF
 		fi
 	done

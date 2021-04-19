9f997c3c85981a6b70debbf7c7e8100bf46ead71
Jorge A. Torres <jorge@0xbad.com>
2021-04-18 20:10:57 -0500

    Install phpMyAdmin when setting up Valet

--- _setup/valet.sh
+++ _setup/valet.sh
@@ -2,3 +2,19 @@
 if ! $(composer global show laravel/valet > /dev/null 2>&1); then
 	composer global require laravel/valet
 fi
+
+if [[ ! $(which mysql) ]]; then
+	brew link mysql@5.7
+fi
+
+
+if [[ ! $(valet links | grep 'phpmyadmin') ]]; then
+	brew install phpmyadmin
+
+	# AllowNoPassword.
+	sed -i '' "s/\['AllowNoPassword'\] = false/\['AllowNoPassword'\] = true/g" "${HOMEBREW_PREFIX}/etc/phpmyadmin.config.inc.php"
+
+	cd "${HOMEBREW_PREFIX}/share/phpmyadmin"
+	valet link
+	valet secure
+fi

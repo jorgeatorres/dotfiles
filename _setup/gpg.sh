ecc1a176a1b53adf503c59730f2a39d4ff83a1a3
Jorge A. Torres <jorge@0xbad.com>
2021-11-21 20:28:46 -0800

    Improvements to GPG script

--- _setup/gpg.sh
+++ _setup/gpg.sh
@@ -1,3 +1,7 @@
+if [[ ! $(which gpg) ]]; then
+	echo "Please install GPG before continuing!"
+	exit 1
+fi
 
 if ! $(gpg --list-keys DD7BA8B00EC20D8C20D55473A5EAF0750A6BCAE9 > /dev/null 2>&1); then
 	eval $(op signin my.1password.com)
@@ -6,7 +10,9 @@ if ! $(gpg --list-keys DD7BA8B00EC20D8C20D55473A5EAF0750A6BCAE9 > /dev/null 2>&1
 	gpg --import /tmp/mykey.key
 
 	op get document "gy3pkac4hbdz7ocrpxaprmtxxm" > /tmp/mykey.key
-	gpg --import /tmp/mykey.key
+	op get item "gy3pkac4hbdz7ocrpxaprmtxxm" --fields "passphrase" > /tmp/mykey.pass
+	gpg --batch --passphrase-file /tmp/mykey.pass --import /tmp/mykey.key
 
 	rm /tmp/mykey.key
+	rm /tmp/mykey.pass
 fi

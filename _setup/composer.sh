9bee6e0b69a29177897bb823024cc4165948a99a
Jorge A. Torres <jorge@0xbad.com>
2021-11-24 19:15:49 -0800

    Rename Composer binaries

--- _setup/composer.sh
+++ _setup/composer.sh
@@ -1,5 +1,15 @@
 
-if [[ ! $(which composer1) ]]; then
-	wget -O "$HOME/.bin/composer1" "https://getcomposer.org/download/1.10.17/composer.phar"
-	chmod +x "$HOME/.bin/composer1"
+if [[ ! $(which composer@1) ]]; then
+	wget -O "$HOME/.bin/composer@1" "https://getcomposer.org/download/1.10.17/composer.phar"
+	chmod +x "$HOME/.bin/composer@1"
+fi
+
+if [[ ! $(which composer@2) ]]; then
+	wget -O "$HOME/.bin/composer@2" "https://getcomposer.org/download/2.1.12/composer.phar"
+	chmod +x "$HOME/.bin/composer@2"
+fi
+
+# Link Composer 2 by default as "composer".
+if [[ ! $(which composer) ]]; then
+	ln -s "$HOME/.bin/composer@2" "$HOME/.bin/composer"
 fi

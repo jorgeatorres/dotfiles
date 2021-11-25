2962cb704146ce03ae0df0cf863140fab53f6d7c
Jorge A. Torres <jorge@0xbad.com>
2021-11-24 19:15:43 -0800

    Rename PHPUnit binaries

--- _setup/phpunit.sh
+++ _setup/phpunit.sh
@@ -1,8 +1,12 @@
 
 
 for i in {5..9}; do
-	if [[ ! $(which "phpunit$i") ]]; then
-		wget -O "$HOME/.bin/phpunit$i" "https://phar.phpunit.de/phpunit-$i.phar"
-		chmod +x "$HOME/.bin/phpunit$i"
+	if [[ ! $(which "phpunit@$i") ]]; then
+		wget -O "$HOME/.bin/phpunit@$i" "https://phar.phpunit.de/phpunit-$i.phar"
+		chmod +x "$HOME/.bin/phpunit@$i"
 	fi
 done
+
+if [[ ! $(which phpunit) ]]; then
+	ln -s "$HOME/.bin/phpunit@9" "$HOME/.bin/phpunit"
+fi

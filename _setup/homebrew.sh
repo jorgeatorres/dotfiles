5e1d045c3225f8027873cf1fa1446c7ffa4a2171
Jorge Torres <jorge@0xbad.com>
2021-12-11 20:38:39 -0300

    Do not source full zshrc when first installing Homebrew

--- _setup/homebrew.sh
+++ _setup/homebrew.sh
@@ -8,12 +8,14 @@ fi
 
 if [[ ! $(which brew)  ]]; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	source ~/.zshrc
 
 	# Homebrew requires Rosetta on Apple Silicon for some packages.
 	if [ "$(uname -m)" = "arm64" ]; then
 		sudo softwareupdate --install-rosetta
 	fi
+
+	if [[ -e "/usr/local/bin/brew" ]]; then eval $(/usr/local/bin/brew shellenv); fi # Homebrew (x86)
+	if [[ -e "/opt/homebrew/bin/brew" ]]; then eval $(/opt/homebrew/bin/brew shellenv); fi # Homebrew (Apple Silicon)
 fi
 
 # Install packages.

f7bc6913694a73d5bf0cab2049c68e237b5346a9
Jorge Torres <jorge@0xbad.com>
2024-02-11 19:02:47 -0800

    Update private.sh

--- _setup/private.sh
+++ _setup/private.sh
@@ -1,6 +1,3 @@
-# Some other scripts stored in an outside repo because of work/secrets.
-if [[ ! -d "$DIR/private.git" ]]; then
-	git clone git@github.com:jorgeatorres/dotfiles-private.git "$DIR/private.git" --quiet;
-fi
+PRIVDIR="$DIR/dotfiles.private"
 
-source "$DIR/private.git/setup.sh"
+source "$PRIVDIR/setup.sh"

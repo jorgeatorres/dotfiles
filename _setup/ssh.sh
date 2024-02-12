1d562714dcd70844cfbb9417eed284c5d5198d1f
Jorge Torres <jorge@0xbad.com>
2024-02-11 18:59:36 -0800

    Modify SSH setup

--- _setup/ssh.sh
+++ _setup/ssh.sh
@@ -1,19 +1,27 @@
 # SSH key setup
-if ! cmp --silent "$DIR/ssh/id_rsa.pub" "$HOME/.ssh/id_rsa.pub"; then
-	mkdir -p "$HOME/.ssh"
-	rm -rfv "$HOME/.ssh/{id_rsa,id_rsa.pub}"
-
-cat <<EOT > "$HOME/.ssh/config"
-Host *
-  UseKeychain yes
-  AddKeysToAgent yes
-  IdentityFile ~/.ssh/id_rsa
+if [[ ! -e "$HOME/.ssh/config"  ]]; then
+	cat <<EOT > "$HOME/.ssh/config"
+	Host *
+	UseKeychain yes
+	AddKeysToAgent yes
+	IdentityFile ~/.ssh/id_rsa
 EOT
-
-	cp "$DIR/ssh/id_rsa.pub" "$HOME/.ssh/"
-	require_1password
-	op get document "7ukxxbzsincz3bqsx6p6iwl32e" > "$HOME/.ssh/id_rsa"
-	chmod 644 "$HOME/.ssh/id_rsa.pub"
-	chmod 600 "$HOME/.ssh/id_rsa"
-	ssh-add -K
 fi
+
+require_1password
+
+op get document "yi3j4cs6ojawzlwp4e763hrwxq" --output /tmp/ssh.pub
+op get document "7ukxxbzsincz3bqsx6p6iwl32e" --output /tmp/ssh.priv
+
+if ! cmp --silent "/tmp/ssh.pub" "$HOME/.ssh/id_rsa.pub"; then
+	mv /tmp/ssh.pub "$HOME/.ssh/id_rsa.pub"
+	chmod 644 "$HOME/.ssh/id_rsa.pub"
+fi
+
+if ! cmp --silent "/tmp/ssh.priv" "$HOME/.ssh/id_rsa"; then
+	mv /tmp/ssh.pub "$HOME/.ssh/id_rsa"
+	chmod 644 "$HOME/.ssh/id_rsa"
+fi
+
+rm -rf /tmp/ssh.{pub,priv}
+ssh-add -K

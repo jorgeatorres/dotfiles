85f0aa8c1a6f9cc3809782fcf5cd0926b2aa1344
Jorge A. Torres <jorge@0xbad.com>
2025-05-04 19:09:00 +0100

    SSH key updates

--- _setup/ssh.sh
+++ _setup/ssh.sh
@@ -4,7 +4,7 @@ if [[ ! -e "$HOME/.ssh/config"  ]]; then
 	Host *
 	UseKeychain yes
 	AddKeysToAgent yes
-	IdentityFile ~/.ssh/id_rsa
+	IdentityFile ~/.ssh/id_ed25519
 EOT
 fi
 
@@ -13,14 +13,14 @@ require_1password
 op get document "yi3j4cs6ojawzlwp4e763hrwxq" --output /tmp/ssh.pub
 op get document "7ukxxbzsincz3bqsx6p6iwl32e" --output /tmp/ssh.priv
 
-if ! cmp --silent "/tmp/ssh.pub" "$HOME/.ssh/id_rsa.pub"; then
-	mv /tmp/ssh.pub "$HOME/.ssh/id_rsa.pub"
-	chmod 644 "$HOME/.ssh/id_rsa.pub"
+if ! cmp --silent "/tmp/ssh.pub" "$HOME/.ssh/id_ed25519.pub"; then
+	mv /tmp/ssh.pub "$HOME/.ssh/id_ed25519.pub"
+	chmod 644 "$HOME/.ssh/id_ed25519.pub"
 fi
 
-if ! cmp --silent "/tmp/ssh.priv" "$HOME/.ssh/id_rsa"; then
-	mv /tmp/ssh.pub "$HOME/.ssh/id_rsa"
-	chmod 644 "$HOME/.ssh/id_rsa"
+if ! cmp --silent "/tmp/ssh.priv" "$HOME/.ssh/id_ed25519"; then
+	mv /tmp/ssh.pub "$HOME/.ssh/id_ed25519"
+	chmod 644 "$HOME/.ssh/id_ed25519"
 fi
 
 rm -rf /tmp/ssh.{pub,priv}

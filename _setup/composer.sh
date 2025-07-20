839e39a52a67f5721f175af1b0411f37ebf61b0f
Jorge A. Torres <jorge@Lagrange.local>
2025-07-20 17:52:06 +0100

    Update Composer versions

--- _setup/composer.sh
+++ _setup/composer.sh
@@ -1,15 +1,13 @@
 
 if [[ ! $(which composer@1) ]]; then
-	wget -O "$HOME/.bin/composer@1" "https://getcomposer.org/download/1.10.17/composer.phar"
+	wget -q -O "$HOME/.bin/composer@1" "https://getcomposer.org/download/1.10.27/composer.phar"
 	chmod +x "$HOME/.bin/composer@1"
 fi
 
 if [[ ! $(which composer@2) ]]; then
-	wget -O "$HOME/.bin/composer@2" "https://getcomposer.org/download/2.1.12/composer.phar"
+	wget -q -O "$HOME/.bin/composer@2" "https://getcomposer.org/download/2.8.10/composer.phar"
 	chmod +x "$HOME/.bin/composer@2"
-fi
 
-# Link Composer 2 by default as "composer".
-if [[ ! $(which composer) ]]; then
-	ln -s "$HOME/.bin/composer@2" "$HOME/.bin/composer"
+	# Link Composer 2 by default as "composer".
+	[[ ! $(which composer) ]] && ln -s "$HOME/.bin/composer@2" "$HOME/.bin/composer"
 fi

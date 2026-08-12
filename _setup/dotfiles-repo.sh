956c82261fa1a9e28fe6d1927646a5ab7874a0d9
Jorge A. Torres <jorge@0xbad.com>
2026-08-12 23:25:44 +0100

    Cloned repo is not bare when run from within repo checkout

--- _setup/dotfiles-repo.sh
+++ _setup/dotfiles-repo.sh
@@ -1,4 +1,4 @@
 if [[ ! -e ${DIR}/.git ]] && [[ -e ~/.ssh/id_ed25519 ]]; then
 	git clone --quiet --bare https://github.com/jorgeatorres/dotfiles.git ${DIR}/.git
-	sed -i '' 's/bare = .*/bare = true/' ${DIR}/.git/config
+	sed -i '' 's/bare = .*/bare = false/' ${DIR}/.git/config
 fi

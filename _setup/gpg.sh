79e0ddb48cceca9a255e48ac50af53bda7181b5a
Jorge A. Torres <jorge@0xbad.com>
2026-08-12 23:25:10 +0100

    Improve GPG and SSH setup

--- _setup/gpg.sh
+++ _setup/gpg.sh
@@ -8,7 +8,5 @@ if ! $(gpg --list-keys 3DC414829569195D1DC48ABE3DBE8B97E5EE00A9 > /dev/null 2>&1
 
 	op read "op://Private/GPG Key/public key" | gpg --import
 
-	op read "op://Private/GPG Key/passphrase" > /tmp/mykey.pass
-	op read "op://Private/GPG Key/private.asc" | gpg --batch --passphrase-file /tmp/mykey.pass --import
-	rm /tmp/mykey.pass
+	op read "op://Private/GPG Key/passphrase" | gpg --batch --pinentry-mode loopback --passphrase-fd 0 --import <(op read "op://Private/GPG Key/private.asc")
 fi
--- _setup/ssh.sh
+++ _setup/ssh.sh
@@ -1,5 +1,6 @@
 # SSH key setup
 mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
 
 if [[ ! -e "$HOME/.ssh/config"  ]]; then
 	cat <<EOT > "$HOME/.ssh/config"
@@ -13,9 +14,12 @@ fi
 
 require_1password
 
-op read "op://Private/SSH Key/public key" > ${HOME}/.ssh/id_ed25519.pub
-op read "op://Private/SSH Key/id_ed25519" > ${HOME}/.ssh/id_ed25519
+(
+	umask 077
+	op read "op://Private/SSH Key/public key" > ${HOME}/.ssh/id_ed25519.pub
+	op read "op://Private/SSH Key/id_ed25519" > ${HOME}/.ssh/id_ed25519
+)
 
 chmod 600 ${HOME}/.ssh/id_ed25519{,.pub}
 
-APPLE_SSH_ADD_BEHAVIOR=macos ssh-add -K
+ssh-add --apple-use-keychain

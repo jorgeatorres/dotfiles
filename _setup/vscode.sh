ae4061786cbbc70c33d290796f21fb4841edba40
Jorge A. Torres <jorge@0xbad.com>
2026-08-13 22:23:20 +0100

    Update vscode config

--- _setup/vscode.sh
+++ _setup/vscode.sh
@@ -1,23 +1,17 @@
-if [[ ! -e "$(gem env gemdir)/bin/solargraph" ]]; then
-	gem install solargraph
+if [[ ! $(which code) ]]; then
+	echo "! code CLI not found, skipping."
+	return
 fi
 
-if [[ ! -e "/usr/local/bin/solargraph" ]]; then
-	sudo ln -s $(gem env gemdir)/bin/solargraph /usr/local/bin
-fi
+CODE_USER="$HOME/Library/Application Support/Code/User"
+mkdir -p "$CODE_USER"
 
-# Visual Studio Code
-mkdir -p "$HOME/Library/Application Support/Code/User"
-ln -sfv "$DIR/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-ln -sfv "$DIR/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+[[ -L "$CODE_USER/settings.json" ]] || ln -sfv "$DIR/vscode/settings.json" "$CODE_USER/settings.json"
+[[ -L "$CODE_USER/keybindings.json" ]] || ln -sfv "$DIR/vscode/keybindings.json" "$CODE_USER/keybindings.json"
 
 INSTALLED_EXTENSIONS="$(code --list-extensions)"
-for EXTENSION in $(cat "$DIR/vscode/extensions-list")
-do
-    if echo "$INSTALLED_EXTENSIONS" | grep -q "$EXTENSION"
-    then
-        true;
-    else
-        code --install-extension "$EXTENSION"
-    fi
+for EXTENSION in $(cat "$DIR/vscode/extensions-list"); do
+	if ! echo "$INSTALLED_EXTENSIONS" | grep -qi "^$EXTENSION$"; then
+		code --install-extension "$EXTENSION"
+	fi
 done
